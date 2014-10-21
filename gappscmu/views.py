6e5bf16cf09103222cb95f61b1ad1ffe18bc8a00
Jacob Zimmerman <zimmerman.jake@gmail.com>
2014-10-20 23:07:41 -0700

    Write the views
    The views are just as simplistic as the URLs that route to them. There's
    really not much to say here.

--- gappscmu/views.py
+++ gappscmu/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response, redirect
+from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout as auth_logout
 
 # Create your views here.
+def home(request):
+    return render_to_response('gappscmu/home.html',
+            {}, RequestContext(request))
+
+def logout(request):
+    auth_logout(request)
+    return redirect('/')
